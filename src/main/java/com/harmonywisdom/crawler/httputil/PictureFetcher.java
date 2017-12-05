313c8ba80495bc80f23a0b01b14bdfd39f78ae41
iamzhouchangjin@163.com <iamzhouchangjin@163.com>
2017-12-05 17:07:34 +0800

    Picture Download From Url

---
+++ src/main/java/com/harmonywisdom/crawler/httputil/PictureFetcher.java
@@ -0,0 +1,52 @@
+package com.harmonywisdom.crawler.httputil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class PictureFetcher {
+	
+	public static String FetchPicture(String url,String savefileName,String savePath){
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpget=new HttpGet(url);
+		String chars[]=url.split("\\.");
+		String suf=".jpg";
+		if(chars.length>1){
+			suf=chars[chars.length-1];
+		}
+		System.out.println("文件后缀"+suf);
+		String finalFileName=savePath+"/"+savefileName+"."+suf;
+		try {
+			CloseableHttpResponse response=httpclient.execute(httpget);
+			HttpEntity entity=response.getEntity();
+			InputStream is=entity.getContent();
+			byte[] outbytes=new byte[1024];
+			int size=0;
+			OutputStream os=new FileOutputStream(new File(finalFileName));
+			while((size=is.read(outbytes))>0){
+				os.write(outbytes, 0, size);
+			}
+			os.flush();
+			os.close();
+			
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
