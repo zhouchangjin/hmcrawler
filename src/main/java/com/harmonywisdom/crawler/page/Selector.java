21fdc335ba4fa368de7496b01287bb2558d786aa
Administrator <Administrator@www.myass.com>
2018-04-25 15:52:14 +0800

    modify

--- src/main/java/com/harmonywisdom/crawler/page/Selector.java
+++ src/main/java/com/harmonywisdom/crawler/page/Selector.java
@@ -1,48 +1,81 @@
 package com.harmonywisdom.crawler.page;
 
-import java.io.StringReader;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Node;
-import org.dom4j.io.SAXReader;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.xml.transform.TransformerException;
+
+import org.apache.xerces.xni.parser.XMLDocumentFilter;
+import org.apache.xpath.XPathAPI;
+import org.cyberneko.html.filters.Purifier;
+import org.cyberneko.html.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
 
 import com.harmonywisdom.crawler.httputil.HtmlFetcher;
 
 
 public class Selector implements IPageSelector{
-	String xmlCont;
-	Document document;
+	String cont;
+	String xmlResult;
+	Document doc;
 	public Selector(String xml) {
-		this.xmlCont=xml;
+		this.cont=xml;
 		initialize();
 	}
 	
 	public void initialize() {
-		SAXReader reader = new SAXReader();
-		try {
-			Document doc=reader.read(new StringReader(this.xmlCont));
-			this.document=doc;
-		} catch (DocumentException e) {
+		
+		DOMParser parser=new DOMParser();
+		 XMLDocumentFilter noop = new Purifier();
+		 XMLDocumentFilter[] filters = { noop };
+		 try {
+			parser.setProperty("http://cyberneko.org/html/properties/filters", filters);
+			parser.setFeature("http://xml.org/sax/features/namespaces", false);  
+			 ByteArrayInputStream bio=new ByteArrayInputStream(cont.getBytes("UTF-8"));
+			 InputSource se=new InputSource(bio);
+			 parser.parse(se);
+			 doc=parser.getDocument();
+		} catch (SAXNotRecognizedException e) {
+			e.printStackTrace();
+		} catch (SAXNotSupportedException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		 
 	}
 
 	@Override
 	public String selectByXpath(String xpath) {
 		// TODO Auto-generated method stub
-		Node node=this.document.selectSingleNode(xpath);
-		return node.getText();
+		Node node;
+		try {
+			node=XPathAPI.selectSingleNode(doc, xpath);
+			return node.getTextContent();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		}
 	}
 	
 	
 	public static void main(String args[]) {
 		
-		JTidyTider tider=new JTidyTider();
-		String res=tider.htmlToXml(HtmlFetcher.FetchHtml("http://www.baidu.com"));
-		Selector parser=new Selector(res);
-		//String tmp=parser.selectByXpath("//div[id='head']");
-		//System.out.println(tmp);
+
+		Selector parser=new Selector(HtmlFetcher.FetchHtml("http://www.51meiyu.cn"));
+		
+		String tmp=parser.selectByXpath("//DIV");
+		System.out.println(tmp);
 	}
 
 }
