0fb56afa150541db7213470b9119108025980a1f
zhouchangjin <iamzhouchangjin@163.com>
2022-08-11 14:42:38 +0800

    test

--- src/main/java/com/harmonywisdom/crawler/page/test/TestBaiduNews.java
+++ src/main/java/com/harmonywisdom/crawler/page/test/TestBaiduNews.java
@@ -12,7 +12,7 @@ public class TestBaiduNews {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HTMLNewsContentExtractor extractor=new HTMLNewsContentExtractor();
-		InputStream stream =HtmlFetcher.getInputSteam("http://www.51meiyu.cn/news/show-1175.html");
+		InputStream stream =HtmlFetcher.getInputSteam("http://www.ffsky.com/Article/bc7368_a15116.aspx");
 		DataInputStream s=new DataInputStream(stream);
 		extractor.setSourceStream(s);
 		extractor.parse();
